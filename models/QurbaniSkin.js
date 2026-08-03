167d1c4495981fa03db6322d9aaf43116b278531
Nazmul <your-email@example.com>
2026-08-03 16:13:42 +0600

    Fix database connection imports and update package dependencies

--- models/QurbaniSkin.js
+++ models/QurbaniSkin.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/db');
+const sequelize = require('../config/db');
 
 const QurbaniSkin = sequelize.define('QurbaniSkin', {
   _id: {
